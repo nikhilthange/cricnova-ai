6fd0ad1f546f0c558fce23328047f88b9d3dd497
Nikhil Thange <nikhilthange075@gmail.com>
2026-03-22 20:28:30 +0530

    some change in port

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = require("./app");
+const PORT = process.env.PORT || 10000;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running...");
 });
