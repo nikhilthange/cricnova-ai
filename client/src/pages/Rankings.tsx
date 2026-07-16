93437f12c816ca99f88251fc8121cf279f5e0df3
Nikhil Thange <nikhilthange075@gmail.com>
2026-07-16 14:36:27 +0530

    Fix incorrect import path in Rankings.tsx causing Vercel build failure

--- client/src/pages/Rankings.tsx
+++ client/src/pages/Rankings.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Trophy, TrendingUp, TrendingDown, Minus } from "lucide-react";
-import { cn } from "../../lib/utils";
+import { cn } from "../lib/utils";
 
 const mockRankings = {
   teams: {
